c417cb3353211693286e77f8254edc72fc9bb15f
Liam Watson <lwatson@shopkeep.com>
2020-10-26 16:27:20 +0000

    adds frontend build

---
+++ build/precache-manifest.6a1ab9aa7e5c226d4c11bc4e3e6796f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a0f41f348c97837ca0f7a6f3db245d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d7f8d6a1ec6d0cc5abc",
+    "url": "/static/css/main.21d9e055.chunk.css"
+  },
+  {
+    "revision": "f24c868740ab0f7c25f5",
+    "url": "/static/js/2.4b8e90bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4b8e90bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d7f8d6a1ec6d0cc5abc",
+    "url": "/static/js/main.df324c96.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
