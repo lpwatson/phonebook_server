bbb29b64bf3d85aaabc63cb3a682dd9c7172f960
Liam Watson <lwatson@shopkeep.com>
2021-02-21 14:42:00 +0000

    exercises 3.15 - 3.17

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const morgan = require('morgan');
 const { response } = require('express');
 const app = express()
 const cors = require('cors')
-const Person = require('./models/person')
+const Person = require('./models/person');
+const { Mongoose } = require('mongoose');
 
 app.use(cors())
 app.use(express.static('build'))
@@ -16,16 +17,34 @@ morgan.token('request-data', function (req, res) {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :request-data'));
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id'})
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
 app.get('/api/persons', (request, response) => {
   Person.find().then(persons => {
     response.json(persons)
   })
 });
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
-  })
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 });
 
 app.get('/info', (request, response) => {
@@ -52,6 +71,30 @@ app.post('/api/persons', (request, response) => {
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
